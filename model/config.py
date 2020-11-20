9f6194a6ff04d50f9708d0e904a1e2c2e3fb7036
pawnesh kumar <pawneshg@rhrk.uni-kl.de>
2020-11-20 19:17:59 +0100

    config changes

--- model/config.py
+++ model/config.py
@@ -18,13 +18,13 @@ data_dir = "/ds/images/MSCOCO2017/"
 train_data_dir = os.path.join(data_dir, "train2017")
 val_data_dir = os.path.join(data_dir, "val2017")
 num_workers = 4
-batch_size = 64
+batch_size = 128
 
 # [model-training]
 pretrain = False
 finetune = False
 start_epoch = 0
-end_epoch = 1
+end_epoch = 100
 checkpoints = True
 save_weights_loc = "/netscratch/kumar/cam/model/weights"
 weights_load_path = None
