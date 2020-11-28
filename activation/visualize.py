34809a37328158d6fac964793706dbe751d6cd64
pawnesh kumar <pawneshg@rhrk.uni-kl.de>
2020-11-28 22:54:43 +0100

    added dynamic number of samples handling.

--- activation/visualize.py
+++ activation/visualize.py
@@ -5,7 +5,8 @@ from sacred_config import ex
 
 def base_vis_template(images, titles, figsize, ncols, nrows, save_path):
     """ Visualization of images """
-    fig, axes = plt.subplots(figsize=figsize, ncols=ncols, nrows=nrows, squeeze=False)
+    plt.rcParams.update({'figure.max_open_warning': 0})
+    fig, axes = plt.subplots(figsize=figsize, ncols=ncols, nrows=nrows, squeeze=False, constrained_layout=True)
     imgs = iter(images)
     titles = iter(titles)
     try:
@@ -18,24 +19,26 @@ def base_vis_template(images, titles, figsize, ncols, nrows, save_path):
         print(err)
     finally:
         fig.savefig(save_path)
+        fig.clf()
     return True
 
 
 @ex.capture
-def coco_activation_map_visualization(_log, model, class_ids, activation_save_path, num_of_cams):
+def coco_activation_map_visualization(_log, model, class_ids, activation_save_path, num_of_cams, num_of_sample_per_class):
     """ Visualize the activation maps of a prediction model. """
     is_success = False
     len_of_labels = int(len(class_ids))
-    nrows = 5
+    nrows = num_of_sample_per_class
     start_ind, ind = 0, 0
+    fig_size = (20, 20)
     data_to_visualize, labels_for_vis_data = construct_visualization_data(model=model,
                                                                           data_to_visualize_func=get_coco_samples_per_class)
-
-    for ind in range(0, len_of_labels, nrows):
+    file_name_itr = iter(labels_for_vis_data[::num_of_cams+1][::nrows])
+    for ind in range(0, len_of_labels*num_of_sample_per_class, nrows):
         end_ind = start_ind + (nrows*(num_of_cams+1))
         is_success = base_vis_template(data_to_visualize[start_ind:end_ind], labels_for_vis_data[start_ind:end_ind],
-                                       figsize=(10, 15), nrows=nrows, ncols=num_of_cams+1,
-                                       save_path=f"{activation_save_path}/activation_map_{ind}.jpg")
+                                       figsize=fig_size, nrows=nrows, ncols=num_of_cams+1,
+                                       save_path=f"{activation_save_path}/{next(file_name_itr)}.jpg")
         if not is_success:
             _log.error("Error in visualization ")
             raise ValueError
@@ -43,7 +46,7 @@ def coco_activation_map_visualization(_log, model, class_ids, activation_save_pa
     _log.debug("start_ind %d", start_ind)
     _log.debug("length of data  %d", len(data_to_visualize))
     if len(data_to_visualize) > start_ind:
-        is_success = base_vis_template(data_to_visualize[start_ind:], labels_for_vis_data[start_ind:], figsize=(10, 15),
+        is_success = base_vis_template(data_to_visualize[start_ind:], labels_for_vis_data[start_ind:], figsize=fig_size,
                                        nrows=nrows, ncols=num_of_cams+1,
-                                       save_path=f"{activation_save_path}/activation_map_{ind+1}.jpg")
+                                       save_path=f"{activation_save_path}/{next(file_name_itr)}.jpg")
     return is_success
